7d63eeb50a4e1537f3089f8aef59d9a3f2bfb852
ikraduya <ikraduya@gmail.com>
2020-11-24 15:35:17 +0700

    add more outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,80 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import logo from './logo.svg';
 import { Jumbotron, Container, Button, Row, Col, FormGroup, Label, Input, Table } from 'reactstrap';
-import Select from 'react-select'
+import Select, { ValueType, ActionMeta } from 'react-select'
 import './App.css';
 
 const queryOptions = [
-  { label: 'A before B', value: 'before' },
-  { label: 'A overlaps B', value: 'overlaps' },
-  { label: 'A contains B', value: 'contains' },
-  { label: 'A starts B', value: 'starts' },
-  { label: 'A finished-by B', value: 'finishedby' },
-  { label: 'A meets B', value: 'meets' },
-  { label: 'A equal B', value: 'equal' },
-  { label: 'A after B', value: 'after' },
-  { label: 'A overlapped-by B', value: 'overlappedby' },
-  { label: 'A during B', value: 'during' },
-  { label: 'A started-by B', value: 'startedby' },
-  { label: 'A finishes B', value: 'finishes' },
-  { label: 'A met-by B', value: 'metby' },
-  { label: 'Most Checking Day', value: 'mostcheckin' },
-  { label: 'Most Cancle Day', value: 'mostcancel' },
-  { label: 'Checkin Per Day', value: 'checkinperday' },
+  { label: 'A before B', value: 'before', num: 0 },
+  { label: 'A overlaps B', value: 'overlaps', num: 1 },
+  { label: 'A contains B', value: 'contains', num: 2 },
+  { label: 'A starts B', value: 'starts', num: 3 },
+  { label: 'A finished-by B', value: 'finishedby', num: 4 },
+  { label: 'A meets B', value: 'meets', num: 5 },
+  { label: 'A equal B', value: 'equal', num: 6 },
+  { label: 'A after B', value: 'after', num: 7 },
+  { label: 'A overlapped-by B', value: 'overlappedby', num: 8 },
+  { label: 'A during B', value: 'during', num: 9 },
+  { label: 'A started-by B', value: 'startedby', num: 10 },
+  { label: 'A finishes B', value: 'finishes', num: 11 },
+  { label: 'A met-by B', value: 'metby', num: 12 },
+  { label: 'Most Checking Day', value: 'mostcheckin', num: 13 },
+  { label: 'Most Cancel Day', value: 'mostcancel', num: 14 },
+  { label: 'Checkin Per Day', value: 'checkinperday', num: 15 },
+  { label: 'Projection', value: 'projection', num: 16 },
+  { label: 'Selection', value: 'checkinperday', num: 17 },
+  { label: 'Union', value: 'checkinperday', num: 18 },
+  { label: 'Temporal Difference', value: 'checkinperday', num: 19 },
+  { label: 'Temporal Join', value: 'checkinperday', num: 20 },
+  { label: 'Transaction-Timeslice', value: 'checkinperday', num: 21 },
+  { label: 'Insert', value: 'checkinperday', num: 22 },
+  { label: 'ts_update', value: 'checkinperday', num: 23 },
+  { label: 'Delete', value: 'checkinperday', num: 24 },
+  { label: 'Modify', value: 'checkinperday', num: 25 },
 ];
 
-function App() {
+type QueryType = {
+  label: string,
+  value: string,
+  num: number
+};
+
+
+type TableItem = {
+  id: number,
+  type: string,
+  adults: number,
+  children: number, 
+  babies: number,
+  checkingDate: Date,
+  checkoutDate: Date,
+  isCanceled: Boolean
+};
+
+const App: React.FC = () => {
+  const [ queryNum, setQueryNum ] = useState(0);
+
+  const tableItems: TableItem[] = [
+    { id: 1, type: "Resort Hotel", adults: 2, children: 0, babies: 0, checkingDate: new Date("2015-07-02"), checkoutDate: new Date("2015-07-07"), isCanceled: Boolean(1) },
+    { id: 2, type: "Resort Hotel", adults: 2, children: 0, babies: 0, checkingDate: new Date("2015-07-02"), checkoutDate: new Date("2015-07-07"), isCanceled: Boolean(1) },
+    { id: 3, type: "Resort Hotel", adults: 2, children: 0, babies: 0, checkingDate: new Date("2015-07-02"), checkoutDate: new Date("2015-07-07"), isCanceled: Boolean(1) },
+  ];
+
+  const renderTableItem = (item: TableItem, index: number) => {
+    return (
+      <tr>
+        <th scope="row">{item.id}</th>
+        <td>{item.type}</td>
+        <td>{item.adults}</td>
+        <td>{item.children}</td>
+        <td>{item.babies}</td>
+        <td>{item.checkingDate.toLocaleDateString()}</td>
+        <td>{item.checkoutDate.toLocaleDateString()}</td>
+        <td>{(item.isCanceled) ? 1 : 0}</td>
+      </tr>
+    );
+  };
+
   return (
     <div className="App" style={{backgroundColor: '#282c34', height: "100vh"}}>
       <Jumbotron style={{textAlign: "center"}}>
@@ -35,15 +86,15 @@ function App() {
             <Select
               options={queryOptions}
               placeholder="Select Query"
-              // onChange={selectedOption => {
-              //   let value = selectedOption!.value;
-              //   console.log(value);
-              // }}
+              onChange={(e: ValueType<QueryType> | null | undefined) => {
+                if (!e) {
+                  e = { label: 'A before B', value: 'before', num: 0 };
+                }
+                const {label, value, num} = e as QueryType;
+                setQueryNum(num);
+              }}
             />
-            <FormGroup className="mt-2">
-              <Label className="text-white" for="inputId" style={{marginBottom: "0"}}>ID A:</Label>
-              <Input type="text" name="input_id" id="inputId" placeholder="Input ID A" />
-            </FormGroup>
+            <ChangableForm query={queryNum}/>
             <Button color="danger" className="mt-1">Execute Query</Button>
           </Col>
           <Col md="9">
@@ -61,36 +112,7 @@ function App() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Resort Hotel</td>
-                  <td>2</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>2015-07-02</td>
-                  <td>2015-07-07</td>
-                  <td>1</td>
-                </tr>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Resort Hotel</td>
-                  <td>2</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>2015-07-02</td>
-                  <td>2015-07-07</td>
-                  <td>1</td>
-                </tr>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Resort Hotel</td>
-                  <td>2</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>2015-07-02</td>
-                  <td>2015-07-07</td>
-                  <td>1</td>
-                </tr>
+                {tableItems.map(renderTableItem)}
               </tbody>
             </Table>
           </Col>
@@ -98,6 +120,28 @@ function App() {
       </Container>
     </div>
   );
+};
+
+const ChangableForm = ({query}: any) => {
+  if (query >= 0 && query <= 12) {  // allen 13
+    return (
+      <FormGroup className="mt-2">
+        <Label className="text-white" for="inputId" style={{marginBottom: "0"}}>ID A:</Label>
+        <Input type="text" name="input_id" id="inputId" placeholder="Input ID A" />
+      </FormGroup>
+    );
+  } else if (query === 13 || query === 14) {  // most checking day or most cancel day
+    return null;
+  } else if (query === 15) {  // checkin per day
+    return (
+      <FormGroup className="mt-2">
+        <Label className="text-white" for="dateSlider" style={{marginBottom: "0"}}>Date Slider:</Label>
+        <Input className="custom-range" type="range" name="input_data_slider" id="dateSlider" placeholder="Select Date" />
+      </FormGroup>
+    );
+  } else {  // query num 16-25
+    return null;
+  }
 }
 
 export default App;
