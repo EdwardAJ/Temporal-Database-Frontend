0b0dcf4416929466237bd61293bd2cf87910a25a
EdwardAJ <13517115@std.stei.itb.ac.id>
2020-11-27 12:24:55 +0700

    Change BASE URL

--- src/utils/axios.utils.ts
+++ src/utils/axios.utils.ts
@@ -1,9 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const customAxios: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://fatanugraha.xyz/api'
 })
 
 export { customAxios }
-
-// https://fatanugraha.xyz/api
